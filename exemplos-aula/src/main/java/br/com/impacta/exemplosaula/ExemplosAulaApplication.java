fc417f2f69d37efa5f0d2e0db4e62034094966c1
Alberto <alberto.santana@aluno.faculdadeimpacta.com.br>
2021-05-29 11:29:26 -0300

    exercicio aula

---
+++ exemplos-aula/src/main/java/br/com/impacta/exemplosaula/ExemplosAulaApplication.java
@@ -0,0 +1,19 @@
+package br.com.impacta.exemplosaula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemplosAulaApplication {
+
+	public static void main(String[] args) {
+
+StringUtils.
+
+		SpringApplication.run(ExemplosAulaApplication.class, args);
+
+
+		
+	}
+
+}
---
+++ exemplos-aula/src/test/java/br/com/impacta/exemplosaula/ExemplosAulaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.impacta.exemplosaula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemplosAulaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
